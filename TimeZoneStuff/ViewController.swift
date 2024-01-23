ca92e8e1b77c81af08b4ff40333ce0a9b66accbb
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-23 11:58:11 +0700

    timezonestuff

--- TimeZoneStuff/ViewController.swift
+++ TimeZoneStuff/ViewController.swift
@@ -11,7 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let today = Date()
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+//        label.text = today.formatted()
+//        label.text = today.formatted() + "\(TimeZone.current.abbreviation() ?? "-")"
+        label.text = today.formatted() + "\(TimeZone.current.identifier)"
+        label.frame = CGRect(x:0,y:0,width: 200,height: 50)
+        label.center = view.center
+        view.addSubview(label)
     }
 
 
